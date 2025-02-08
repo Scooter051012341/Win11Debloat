b2f6c9101a791b79d3a85a13ec70dc5f8c3185b9
Scooter051012341 <137853695+Scooter051012341@users.noreply.github.com>
2025-02-08 10:06:33 +0000

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,1 +1,2 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
+
+owerShell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0Win11Debloat.ps1""' -Verb RunAs}"
